3cba8d5609001c11be607cba90cd94e7ab780fb5
Cara Zellner <carajeanine@gmail.com>
2020-10-05 22:46:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ",]
+board = [" ", " ", " ", " "," ", " " ," " ," " ," "]
+
+def display_board
+  puts " " " " " "
+  -----------
+end
